caea20348adbc3674fbd861df44021a3f4c1575b
Beterraba <ls6182315@gmail.com>
2024-05-27 18:14:11 -0300

    cria os inputs para inserir informacoes sobre a conta banco e gera uma saudacao no final

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,7 +1,27 @@
 package src;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o número da Agência !");
+        int numAgencia = sc.nextInt();
+        System.out.println("Agora,digite sua agencia com digito! XXX-X");
+        String agencia = sc.next();
+        System.out.println("Agora,digite seu nome:");
+        String nome = sc.next();
+        sc.nextLine();
+        System.out.println("Agora digite o valor depositado que ficara disponivel na sua conta");
+        double saldo = sc.nextDouble();
+
+        ContaBanco novaConta = new ContaBanco(numAgencia,agencia,nome,saldo);
+
+        novaConta.saudacao();
 
     }
 }
